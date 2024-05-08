3bb2b4e0a16d4a4c591bf43cb23b5ef2e217cbe2
Steven Candelario <stevencandelariof@gmail.com>
2024-05-07 22:10:58 -0400

    Se ha inyectado el contexto

--- ProyectoTecnicos/Program.cs
+++ ProyectoTecnicos/Program.cs
@@ -1,36 +1,43 @@
+using Microsoft.EntityFrameworkCore;
 using ProyectoTecnicos.Components;
+using ProyectoTecnicos.DAL;
 
-namespace ProyectoTecnicos
+namespace ProyectoTecnicos;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+
+        //Obtenemos el ConStr
+        var ConStr = builder.Configuration.GetConnectionString("ConStr");
+
+        //Agregamos el contexto al builder
+        builder.Services.AddDbContext<Contexto>(Options => Options.UseSqlite(ConStr));
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            builder.Services.AddRazorComponents()
-                .AddInteractiveServerComponents();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseStaticFiles();
-            app.UseAntiforgery();
-
-            app.MapRazorComponents<App>()
-                .AddInteractiveServerRenderMode();
-
-            app.Run();
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+        app.Run();
     }
 }
