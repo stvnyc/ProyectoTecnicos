baa0ce0d750a47b3bb2253e065b404e2076d8d66
Steven Candelario <stevencandelariof@gmail.com>
2024-05-07 21:59:29 -0400

    Se ha creado el modelo Tecnicos

---
+++ ProyectoTecnicos/Models/Tecnicos.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoTecnicos.Models;
+
+public class Tecnicos
+{
+    [Key]
+    public int TecnicoId { get; set; }
+    [Required(ErrorMessage ="El campo Nombres es olbigatorio")]
+    public string? Nombres { get; set; }
+    [Required(ErrorMessage ="El campo Sueldo es obligatorio")]
+    public decimal? SueldoHora { get; set; }
+}
