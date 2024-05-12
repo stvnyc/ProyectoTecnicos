9f78bb47ead606d137536819bba108d840b02342
Steven Candelario <stevencandelariof@gmail.com>
2024-05-11 20:00:43 -0400

    Se han agregado mensajes de error

--- ProyectoTecnicos/Models/Tecnicos.cs
+++ ProyectoTecnicos/Models/Tecnicos.cs
@@ -6,8 +6,12 @@ public class Tecnicos
 {
     [Key]
     public int TecnicoId { get; set; }
-    [Required(ErrorMessage ="El campo Nombres es olbigatorio")]
+
+    [Required(ErrorMessage ="El campo Nombres es olbigatorio!")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "El Nombre debe contener solo letras.")]
     public string? Nombres { get; set; }
-    [Required(ErrorMessage ="El campo Sueldo es obligatorio")]
+
+    [Required(ErrorMessage ="El campo Sueldo es obligatorio!")]
+    [Range(0.01, float.MaxValue, ErrorMessage ="Ingrese un valor mayor a 0")]
     public decimal? SueldoHora { get; set; }
 }
