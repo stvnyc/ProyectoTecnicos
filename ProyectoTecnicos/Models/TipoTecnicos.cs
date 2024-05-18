133dc565e1cb028873f71370ca56340ed85000e0
Steven Candelario <stevencandelariof@gmail.com>
2024-05-18 14:58:52 -0400

    Agregando el modelo TipoTecnicos

---
+++ ProyectoTecnicos/Models/TipoTecnicos.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoTecnicos.Models;
+
+public class TipoTecnicos
+{
+    [Key]
+    public int TipoId { get; set; }
+
+    [Required(ErrorMessage = "El campo descripción es requerido")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Este campo no debe contener caracteres especiales")]
+    public string? Descripcion { get; set; }
+}
